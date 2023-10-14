9a3e1346558e59da5920521f34db83cfa7bc277d
Kyle <kyleryanjs@gmail.com>
2023-10-14 13:19:51 -0700

    processing sign in, register, and update user requests from frontend (react-face-detection)

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -30,9 +33,16 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+  // bcrypt.compare('bacon', hash, function(err, res) {
+  //   console.log('first guess', res)
+  // });
+  // bcrypt.compare('veggies', hash, function(err, res) {
+  //   console.log('second guess', res)
+  // });
+
   if (req.body.email === database.users[0].email &&
       req.body.password === database.users[0].password) {
-    res.json('success');
+    res.json(database.users[0]);
   } else {
     res.status(400).json('error logging in');
   }
@@ -40,6 +50,10 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+
+  // bcrypt.hash(password, null, null, function(err, hash) {
+  //   console.log(hash);
+  // });
 
   database.users.push({
     id: '125',
@@ -88,6 +102,6 @@ app.put('/image', (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log('app is runnning on port 3000');
+app.listen(3001, () => {
+  console.log('app is runnning on port 3001');
 })

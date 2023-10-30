a19146f5d9d145b873818e7a1c20c0ed3c9ad193
Kyle <kyleryanjs@gmail.com>
2023-10-29 19:26:21 -0700

    validate register and signin form data + move clarifai api call to server

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,46 @@
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const PAT = 'PAT';
+  const USER_ID = 'USER_ID';
+  const APP_ID = 'APP_ID';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
+          }
+        }
+      }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
+
+const handleApiCall = (req, res) => {
+  fetch('https://api.clarifai.com/v2/models/face-detection/outputs', returnClarifaiRequestOptions(req.body.input))
+    .then(result => result.json())
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
@@ -11,5 +54,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 }
--- server.js
+++ server.js
@@ -25,12 +25,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(3001, () => {
   console.log('app is runnning on port 3001');
